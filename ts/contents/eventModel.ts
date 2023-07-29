790552fa44070432e1c611937b4e711c5c565c88
kazu0221 <kirito-sao.com3290@ezweb.ne.jp>
2023-07-29 15:45:34 +0900

    [add]eventpoint追加

--- ts/contents/eventModel.ts
+++ ts/contents/eventModel.ts
@@ -1,10 +1,24 @@
+import { query } from "./../lib/database"
 
 //TODO: イベントポイント
-export function addEventPoint(userId: number, baeePoint: number, randomPoint: number)
+export async function addEventPoint(userId: number, basePoint: number, randomPoint: number)
 {
 	try
 	{
+		let user = await query("SELECT * FROM EventPoint WHERE id = ?", userId);
+		if (user.length <= 0) {
+			let create = await query("INSERT INTO EventPoint (id, point) VALUES(?, ?)", [userId, 0]);
+		}
+		
 		//NOTE: userModelのaddMoneyを参考にするとよい
+		let addAmount = basePoint + Math.floor(Math.random() * randomPoint);
+		let result = await query("UPDATE EventPoint SET point = point + ? WHERE id = ?", [addAmount, userId]);
+
+		//キャッシュの更新が必要ならそうする
+		//
+
+		//
+		return [addAmount];
 	}
 	catch(ex)
 	{
